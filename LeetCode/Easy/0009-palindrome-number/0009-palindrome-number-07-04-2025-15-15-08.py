c099f5c2036b2f707c4e333925df6b70f4ae9bc8
EGPeat <87342021+EGPeat@users.noreply.github.com>
2025-07-04 15:15:07 +0700

    0009-palindrome-number8 ms (52.19%)17.8 MB (39.6%)Easy

---
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number-07-04-2025-15-15-08.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x is None:
+            return False
+        if x < 0:
+            return False
+        list_x = list(str(x))
+        if len(list_x) == 1:
+            return True
+        len_num = len(list_x)
+        for i in range(len_num//2):
+            if list_x[i] != list_x[len_num-i-1]:
+                return False
+        return True
