42f7d25e430decde86add7f964df15b30fa50e2f
EGPeat <87342021+EGPeat@users.noreply.github.com>
2025-07-06 16:37:26 +0700

    0013-roman-to-integer11 ms (15.29%)17.8 MB (39.99%)Easy

---
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer-07-06-2025-16-37-26.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        conversion_dict = {
+            'M': 1000, 'D': 500, 'C': 100, 
+            'L': 50, 'X':10, 'V': 5, 'I': 1
+            }
+        output_number = 0
+        memory = 0
+        list_str = [conversion_dict[item] for item in list(s)]
+        while list_str:
+            new_number = list_str.pop()
+            if new_number < memory:
+                output_number -= new_number
+            else:
+                output_number += new_number
+            memory = new_number
+        return output_number
