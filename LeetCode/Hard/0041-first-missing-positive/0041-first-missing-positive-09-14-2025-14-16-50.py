03ac2e67fc050c792e0474dbef1d8905c932f178
EGPeat <87342021+EGPeat@users.noreply.github.com>
2025-09-14 14:16:49 +0700

    0041-first-missing-positive45 ms (52.41%)28.8 MB (97.24%)Hard

---
+++ LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive-09-14-2025-14-16-50.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        i, n = 0, len(nums)
+        while i < n:
+            j = nums[i] - 1
+            if abs(nums[i]) < n and nums[i] != nums[j] and nums[i] >= 0:
+                nums[i], nums[j] = nums[j], nums[i]  # swap
+            else:
+                i += 1
+        for i in range(n):
+            if nums[i] != (i + 1):
+                return i + 1
+
+        return len(nums) + 1
