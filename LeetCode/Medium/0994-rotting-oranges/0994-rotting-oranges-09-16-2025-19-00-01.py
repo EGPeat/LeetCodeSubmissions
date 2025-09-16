2311ded8d4096a1fa86169d35f7dc440e993c66b
EGPeat <87342021+EGPeat@users.noreply.github.com>
2025-09-16 19:00:02 +0700

    0994-rotting-oranges3 ms (81.32%)18 MB (5.72%)Medium

---
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges-09-16-2025-19-00-01.py
@@ -0,0 +1,44 @@
+class Solution:
+    def orangesRotting(self, mat: List[List[int]]) -> int:
+    
+        r, c = len(mat), len(mat[0])
+        maxval = 0
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        out = [[-1 for _ in range(len(mat[0]))] for _ in range(len(mat))]
+        queue = deque()
+        for x in range(len(mat)):
+            for y in range(len(mat[0])):
+                if mat[x][y] == 2:
+                    queue.append((x, y, 0))
+                    out[x][y] = 0
+                if mat[x][y] == 0:
+                    out[x][y] = 0
+        
+        #print(queue)
+
+        while queue:
+            row, col, height = queue.popleft()
+            #print((row, col), visited[row][col])
+            for neighbor in get_neighbors((row, col), r, c, mat, out):
+                queue.append((neighbor[0], neighbor[1], out[row][col]))
+                out[neighbor[0]][neighbor[1]] = out[row][col] + 1
+                maxval = max(maxval, out[row][col] + 1)
+        flattened_list = [item for sublist in out for item in sublist]
+        min_element = min(flattened_list)
+        if min_element == -1:
+            return -1
+        return maxval
+
+
+
+def get_neighbors(coord, r, c, mat, out):
+    row, col = coord
+    delta_row = [-1, 0, 1, 0]
+    delta_col = [0, 1, 0, -1]
+    res = []
+    for i in range(len(delta_row)):
+        neighbor_row = row + delta_row[i]
+        neighbor_col = col + delta_col[i]
+        if 0 <= neighbor_row < r and 0 <= neighbor_col < c and mat[neighbor_row][neighbor_col] == 1 and out[neighbor_row][neighbor_col] == -1:
+            res.append((neighbor_row, neighbor_col))
+    return res
