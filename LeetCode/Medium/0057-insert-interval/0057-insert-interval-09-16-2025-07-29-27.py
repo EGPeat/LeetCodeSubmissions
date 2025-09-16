20abfcc08a0bc00330620a2e1997931f2d5a5a9b
EGPeat <87342021+EGPeat@users.noreply.github.com>
2025-09-16 07:29:29 +0700

    0057-insert-interval7 ms (7.09%)19.8 MB (19.53%)Medium

---
+++ LeetCode/Medium/0057-insert-interval/0057-insert-interval-09-16-2025-07-29-27.py
@@ -0,0 +1,17 @@
+class Solution:
+    def insert(self, intervals, new_interval):
+        intervals.append(new_interval)
+        mergedIntervals = []
+        intervals = sorted(intervals, key=lambda interval: interval[0])
+        while len(intervals) > 1:
+            tmp1 = intervals[0]
+            tmp2 = intervals[1]
+            if tmp1[1] >= tmp2[0]:
+                tmp1[1] = max(tmp2[1], tmp1[1])
+                tmp1[0] = min(tmp2[0], tmp1[0])
+                intervals.pop(1)
+            else:
+                mergedIntervals.append(tmp1)
+                intervals.pop(0)
+        mergedIntervals.append(intervals[0])
+        return mergedIntervals
