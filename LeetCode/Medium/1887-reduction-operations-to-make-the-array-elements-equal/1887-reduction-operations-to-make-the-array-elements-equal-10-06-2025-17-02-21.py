45e75b764bd626324b65459490743c755b46ef01
EGPeat <87342021+EGPeat@users.noreply.github.com>
2025-10-06 17:02:22 +0700

    1887-reduction-operations-to-make-the-array-elements-equal131 ms (39.88%)23.4 MB (73.01%)Medium

---
+++ LeetCode/Medium/1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal-10-06-2025-17-02-21.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        amount_of_operations = 0
+        n = len(nums)
+        nums.sort(reverse=True)
+        maxed_numbers = 0
+        i = 0
+        if nums[0] == nums[-1]:
+            return 0
+        while i < n:
+            max_val = nums[i]
+            if nums[i] == nums[-1]:
+                return amount_of_operations
+            while i < n and nums[i] == max_val:
+                maxed_numbers += 1
+                i += 1
+            amount_of_operations += maxed_numbers
+
+
+
+        return amount_of_operations - maxed_numbers
