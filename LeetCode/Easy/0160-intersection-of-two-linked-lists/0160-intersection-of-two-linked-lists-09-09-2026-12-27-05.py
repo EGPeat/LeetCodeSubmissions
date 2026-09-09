cd56eea819a90de709379d048d6d374589847cb2
EGPeat <87342021+EGPeat@users.noreply.github.com>
2026-09-09 12:27:06 +0700

    0160-intersection-of-two-linked-lists100 ms (94.76%)40.8 MB (5.18%)Easy

---
+++ LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists-09-09-2026-12-27-05.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        set_nodes= set()
+        while headA and headB:
+            if headA in set_nodes:
+                return headA
+            set_nodes.add(headA)
+            headA = headA.next
+            if headB in set_nodes:
+                return headB
+            set_nodes.add(headB)
+            headB = headB.next
+        
+        while headA:
+            if headA in set_nodes:
+                return headA
+            set_nodes.add(headA)
+            headA = headA.next
+        while headB:
+            if headB in set_nodes:
+                return headB
+            set_nodes.add(headB)
+            headB = headB.next
+        
+        else:
+            return None
